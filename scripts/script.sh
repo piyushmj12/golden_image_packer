8bf3e74e6cf8f2cf865031e5f51c4439749df524
Prajwal Raju Hatwar <hatwarprajwal@gmail.com>
2022-08-05 11:42:29 +0530

    Update script.sh

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,4 +1,12 @@
+
+/*
 # Put your code here
 #!/bin/bash
 sudo yum -y update
 sudo yum install -y httpd
+*/
+echo Installing Updates
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install -y nginx
+    

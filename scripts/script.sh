4628eb3c62173e601ceab3a8adfc055c992ec4eb
Prajwal Raju Hatwar <hatwarprajwal@gmail.com>
2022-08-05 11:43:14 +0530

    Update script.sh

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,10 +1,10 @@
 
-/*
+
 # Put your code here
 #!/bin/bash
-sudo yum -y update
-sudo yum install -y httpd
-*/
+# sudo yum -y update
+# sudo yum install -y httpd
+
 echo Installing Updates
 sudo apt-get update
 sudo apt-get upgrade -y

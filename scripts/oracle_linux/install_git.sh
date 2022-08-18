0352b101c49914ad05d096dac621128db48d2637
piyushmj12 <76661538+piyushmj12@users.noreply.github.com>
2022-08-18 15:06:33 +0530

    Update install_git.sh

--- scripts/oracle_linux/install_git.sh
+++ scripts/oracle_linux/install_git.sh
@@ -1,1 +1,9 @@
-
+cd /usr/src
+wget https://mirrors.edge.kernel.org/pub/software/scm/git/git-2.36.1.tar.gz
+tar xzf git-2.36.1.tar.gz
+cd git-2.36.1
+make prefix=/usr/local/git all
+make prefix=/usr/local/git install
+echo "export PATH=$PATH:/usr/local/git/bin" >> /etc/bashrc
+source /etc/bashrc
+git --version

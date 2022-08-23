e9d72564c9f1b74628640ecffc50e97f94c95c8a
Prajwal Raju Hatwar <hatwarprajwal@gmail.com>
2022-08-23 16:21:41 +0530

    Update and rename install_java.sh to install_java_ubuntu.sh

--- scripts/ubuntu/install_java.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo apt-get update -y 
-sudo apt install default-jdk -y
-sudo apt install default-jre -y
-
-# sudo apt install oracle-java11-installer -y
-# sudo apt install default-jre
-# sudo apt install default-jdk
-
---
+++ scripts/ubuntu/install_java_ubuntu.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo apt update && sudo apt upgrade -y
+sudo apt install default-jdk -y
+sudo apt update -y
+sudo apt install default-jre -y
